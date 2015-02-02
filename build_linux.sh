316039a328764317ebfe5cf996563f464e915e5c
Nika Gerson Lohman <nikagl@gmail.com>
2015-02-02 23:35:39 +0100

    Added 7zip to build_dos and changed README_FIRST
    Added 7zip to build_dos command and changed README_FIRST.txt to
    README.md for both build_dos.cmd and build_linux.sh

--- build_linux.sh
+++ build_linux.sh
@@ -2,7 +2,7 @@
 wine ~/.wine/dosdevices/c\:/Python27/python.exe ~/.wine/dosdevices/c\:/pyinstaller-2.0/pyinstaller.py .toolkit.py --onefile
 mv dist/.toolkit.exe toolkit.exe
 
-7z a ${PWD##*/}.zip bin/ external2internal/ system_image/ README_FIRST.txt .toolkit.py toolkit.sh toolkit.exe changelog.txt
+7z a ${PWD##*/}.zip bin/ external2internal/ system_image/ README.md .toolkit.py toolkit.sh toolkit.exe changelog.txt
 mv ${PWD##*/}.zip ../
 
 rm -rf build
