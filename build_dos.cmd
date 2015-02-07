902a69f3eae05c95b7d1881b77ba962fdeb0be20
Nika Gerson Lohman <nikagl@gmail.com>
2015-02-07 19:43:18 +0100

    Fixed toolkit building
    Added adb kill-server so toolkit.exe does not remains locked when
    executed

--- build_dos.cmd
+++ build_dos.cmd
@@ -1,4 +1,7 @@
 @echo off
+CD /D %~dp0
+adb kill-server
+del toolkit.exe
 C:\Python27\python.exe C:\pyinstaller-2.1\pyinstaller.py .toolkit.py --onefile
 move dist\.toolkit.exe toolkit.exe
 "C:\Program Files (x86)\7-Zip\7z.exe" a acer_iconia_toolkit_v0.9.2.zip bin\ external2internal\ system_image\ README.md .toolkit.py toolkit.sh toolkit.exe changelog.txt
